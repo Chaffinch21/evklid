e0d61ddf1f9bb9b706f9cfb43f1d83562090d718
Chaffinch21 <lena_431@mail.ru>
2021-05-20 10:50:00 +0700

    evclid first version

---
+++ script/script.js
@@ -0,0 +1,33 @@
+  const sliderEl = document.querySelector('.swiper-container-js');
+  const questionBtn = document.querySelectorAll('.questions__btn-js');
+  const questionItemEl = document.querySelectorAll('.questions__item-js');
+  
+
+  let mySwiper = new Swiper (sliderEl, {
+    loop: true,
+    spaceBetween: 10,
+    // autoplay: {
+    //   delay: 3000,
+    // },
+
+    effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: 'true',
+    },
+  });
+
+    for(i=0; i<questionItemEl.length; i++){
+      questionItemEl[i].addEventListener('click', function(ev){
+      ev.preventDefault();
+      this.children[1].classList.toggle('visible');
+      this.children[0].children[1].classList.toggle('open');
+      })
+    }
+      
+   
